6c4cd6e3472fb25d682260c5dbd983c1e528263e
Alejandro Estringana <estringana@gmail.com>
2016-06-06 18:24:58 +0100

    Configure email address

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'do-not-reply@theporra.com', 'name' => 'Theporra'],
 
     /*
     |--------------------------------------------------------------------------
