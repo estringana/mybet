0a8e116ae3b1cf9a203bbbca8d750c5235c2c99e
Alejandro Estringana <estringana@gmail.com>
2016-06-13 19:52:59 +0100

    Add own goals mark

--- resources/views/championship/pages/matches/goals.blade.php
+++ resources/views/championship/pages/matches/goals.blade.php
@@ -1,5 +1,12 @@
 <div class="goals">
     @foreach($goals as $goal)
-        <i class="fa fa-futbol-o" aria-hidden="true"></i> {{$goal->player->name}} {!!FlagIcon::get($goal->player->team->short_code,$goal->player->team->name)!!}</br>
+        <i class="fa fa-futbol-o" aria-hidden="true"></i> {{$goal->player->name}} {!!FlagIcon::get($goal->player->team->short_code,$goal->player->team->name)!!}
+        @if($goal->own_goal)
+            (own goal)
+        @endif
+        @if($goal->penalty)
+            (p)
+        @endif
+        </br>
     @endforeach
 </div>
