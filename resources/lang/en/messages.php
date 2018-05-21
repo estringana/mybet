f81970f1c42054224b78b3ba27992fa11617514e
Alejandro Estringana Ruiz <alejandro.ruiz@theiconic.com.au>
2018-05-21 20:10:02 +1000

    Change wording

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -99,7 +99,7 @@ return array (
   'Matches has been partially saved' => 'Matches has been partially saved',
   'Players has been partially saved' => 'Players has been partially saved',
   'Teams has been partially saved' => 'Teams has been partially saved',
-  'Remember you need to complete this before the date given above' => 'Remember you need to complete this before the date given above',
+  'Remember you need to complete this before the date given above' => 'Remember you need to complete this before the date given on the rules given on the homepage',
   'All you need to do now is to finish your bet' => 'All you need to do now is to finish your bet',
   'Manage your bet' => 'Manage your bet',
 
--- resources/lang/es/rules.php
+++ resources/lang/es/rules.php
@@ -3,31 +3,49 @@
 return array (
   'Bets' => 
   array (
-    '001' => 'No sera posible hacer cambios en las apuesta a partir del 9 de Junio a las 18:00(Horario español), 5pm(Horario UK)',
+    '001' => 'No sera posible hacer cambios en las apuesta a partir del 12 de Junio a las 18:00(Horario español), 5pm(Horario UK) o 2am(13 Junio - horario Sydney)',
     '002' => 'Tu apuesta deberá estar terminada para entonces. En caso de no estar terminada, el dinero no será devuelto',
     '003' => 'No será posible hacer ningún cambio después de ese momento',
     '004' => 'Si uno de tus jugadores se lesiona cuando se ha cerrado el tiempo de hacer cambios, no será posible cambiarlo',
     'title' => 'Apuestas',
   ),
+  'Payments' => 
+  array (
+    '001' => 'Cada jugador deberá pagar 10€, £9 o AU$16 para unirse al campeonato',
+    '002' => 'Los pagos deben hacerse directamente a la persona que conozcas en la organización de theporra(o a traves del amigo que te hablo de la porra)',
+    '003' => 'Los premios serán pagados de la misma forma pero en sentido inverso',
+    '004' => 'Fecha limite para pagos es el 11 de Junio',
+    'title' => 'Pagos',
+  ),
+  'Website' => 
+  array (
+    '001' => 'No se mostrará ninguna apuesta al resto de jugadores hasta el día 13 de Junio a las 20:00(Horario español), 7pm (Horario UK) o 2am(14 Junio - Horario Sydney)',
+    '002' => 'Todas las apuestas serán enviadas en un fichero PDF. Esto lo hacemos para demostrar que ninguna apuesta cambia una vez cerrado el plazo. No queremos que la gente piense cosas que no son.',
+    '003' => 'Una vez que el campeonato empiece será posible seguir los partidos y la clasificación en la web',
+    '004' => 'La clasificación la mantendremos actualizada',  
+    'title' => 'Website',
+  ),
+  'Prizes' => 
+  array (
+    '001' => '1ro - 70% del dinero',
+    '002' => '2do 20% del dinero',
+    '003' => '3ro 10% del dinero',
+    '004' => 'Todo el dinero recogido de las apuestas será utilizado para premios',
+    '005' => 'El total de dinero recogido será publicado una vez que el plazo de las apuesta sea cerrado',
+    '006' => 'En caso de que dos jugadores acaben con los mismo puntos, los premios serán combinados y divididos por la cantidad de jugadores que tengan los mismo puntos. Por ejemplo, si dos jugadores terminan con los mismos puntos en segunda posición, ambos se repartirán el 30% (10% del tercer premio y 20% del segundo). Si los cuatro primeros acabasen con los mismos puntos, todo el dinero sería repartido entre ellos, etc.',
+    'title' => 'Premios',
+  ),  
   'Clarifications' => 
   array (
-    '001' => 'El sistema de apuestas no es estricto. Con esto queremos decir que no tienes que pasar los equipo que deberían pasar si miras tus rondas anteriores. Por ejemplo, puedes poner que un equipo pierde todos los partidos pero aún así lo pones en la siguiente ronda. Otro ejemplo podría ser que pases todos los equipo de un mismo grupo a la siguiente fase',
+    '001' => 'El sistema de apuestas no es estricto. Con esto queremos decir que no tienes que pasar los equipos que deberían pasar si miras tus rondas anteriores. Por ejemplo, puedes poner que un equipo pierde todos los partidos pero aún así lo pasas en la siguiente ronda. Otro ejemplo podría ser que pases todos los equipo de un mismo grupo a la siguiente fase',
     '002' => 'Se puede coger 8 jugadores del mismo equipo. La única restricción es que no puedes coger al mismo jugador mas de una vez.',
     'title' => 'Aclaraciones',
   ),
   'Goals' => 
   array (
-    '001' => 'Utilizaremos los goles que la UEFA de por validos. En caso de goles en propia puerta o cosas parecidas, contaremos el gol solamente si la UEFA lo cuenta para pichihi.',
+    '001' => 'Utilizaremos los goles que la FIFA de por validos. En caso de goles en propia puerta o cosas parecidas, contaremos el gol solamente si la FIFA lo cuenta para pichihi.',
     'title' => 'Goles',
-  ),
-  'Payments' => 
-  array (
-    '001' => 'Cada jugador deberá pagar 10€ o £8 para unirse al campeonato',
-    '002' => 'Los pagos deben hacerse directamente a la persona que conozcas en la organización de theporra',
-    '003' => 'Los premios serán pagados de la misma forma pero en sentido inverso',
-    '004' => 'Fecha limite para pagos es el 8 de Junio',
-    'title' => 'Pagos',
-  ),
+  ),  
   'Points' => 
   array (
     '001' => '1 punto por cada partido acertado en la primera fase',
@@ -39,24 +57,5 @@ return array (
     '007' => '2 puntos si aciertas el subcampeón',
     '008' => '3 puntos si aciertas el campeón',
     'title' => 'Puntos',
-  ),
-  'Prizes' => 
-  array (
-    '001' => '1ro - 70% del dinero',
-    '002' => '2do 20% del dinero',
-    '003' => '3ro 10% del dinero',
-    '004' => 'Todo el dinero recogido de las apuestas será utilizado para premios',
-    '005' => 'El total de dinero recogido será publicado una vez que tengamos todas las apuestas',
-    '006' => 'En caso de que dos jugadores acaben con los mismo puntos, los premios serán combinados y divididos por la cantidad de jugadores que tengan los mismo puntos. Por ejemplo, si dos jugadores terminan con los mismos puntos en segunda posición, ambos se repartirán el 30% (10% del tercer premio y 20% del segundo). Si los cuatro primeros acabasen con los mismos puntos, todo el dinero sería repartido entre ellos, etc.',
-    'title' => 'Premios',
-  ),
-  'Website' => 
-  array (
-    '001' => 'No se mostrará ninguna apuesta al resto de jugadores hasta el día 10 de Junio a las 20:00(Horario español), 7pm (Horario UK)',
-    '002' => 'Todas las apuestas serán enviadas en un fichero PDF. Esto lo hacemos para demostrar que ninguna apuesta cambia una vez cerrado el plazo. No queremos que la gente piense cosas que no son.',
-    '003' => 'Una vez que el campeonato empiece será posible seguir los partidos y la clasificación en la web',
-    '004' => 'La clasificación la mantendremos actualizada',
-    '005' => 'Todo el mundo podrá actualizar los resultados. En principio todo el mundo podrá proponer resultados. No hacer el gamba ya que mantendremos quien ha propuesto que resultado... Para que un resultado sea aceptado como valido, 4 jugadores tendrán que aprobarlo.',
-    'title' => 'Website',
-  ),
+  ),  
 );
