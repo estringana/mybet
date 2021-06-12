ed083419e5ca096df5e37a79d0a9b6af7ad71204
Alejandro Estringana <alejandro.estringana@hdivsecurity.com>
2021-06-12 18:35:57 +0200

    Remove messages from previous championship

--- resources/views/championship/pages/table.blade.php
+++ resources/views/championship/pages/table.blade.php
@@ -4,9 +4,5 @@
     <div class="jumbotron">
       <h1>{{trans('messages.Table of')}} {{$championship->name}}</h1>
     </div>
-    <div class="alert alert-info" role="alert">
-        {{trans('messages.We have started counting points from qualified teams. Check on the next link')}}:
-        <a href="/rounds">{{trans('messages.Qualified teams')}}</a>
-    </div>
     @include('championship.table.table')
 @stop
